dda73deec8540424f452150249bd33489bae4613
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-07 14:38:33 -0500

    Add basic interface to 'Memory' class in 'memory' header file

--- include/memory.hpp
+++ include/memory.hpp
@@ -1,10 +1,28 @@
 #ifndef CHIP8_MEMORY_H
 #define CHIP8_MEMORY_H
 
+#include <array>
+#include <cassert>
+
 namespace chip8 {
-class Memory {
+
+template <typename T, std::size_t sz> class Memory {
+  public:
     Memory() = default;
     ~Memory() = default;
+
+    T &operator[](std::size_t i) {
+        assert(i < sz);
+        return m_buffer[i];
+    }
+
+    T operator[](std::size_t i) const {
+        assert(i < sz);
+        return m_buffer[i];
+    }
+
+  private:
+    std::array<T, sz> m_buffer;
 };
 } // namespace chip8
 
