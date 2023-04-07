bc27e54870d1374cfe730df976e657d31bb87a8d
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-07 18:16:52 -0500

    Update memory (#2)
    * Update 'Memory' class to use C-style array
    
    * Remove #include <array> statement in 'memory' header file
    
    * Define methods for getting pointers to the beginning and end of 'Memory' buffer
    
    * Add constructor for initializing 'Memory' class with default value
    
    * 'Memory' class throws out_of_range exception on index value
    
    * Add operator& methods for const and non-const versions of 'Memory' class
    
    * const version of operator[] in 'Memory' class now calls non-const operator[] internally
    
    * Add size() method to 'Memory' class
    
    * Update header guard in 'memory' header file
    
    * Add tests for 'Memory' class
    
    * Update makefile

--- include/memory.hpp
+++ include/memory.hpp
@@ -1,28 +1,45 @@
-#ifndef CHIP8_MEMORY_H
-#define CHIP8_MEMORY_H
+#ifndef CHIP8_MEMORY_HPP
+#define CHIP8_MEMORY_HPP
 
-#include <array>
 #include <cassert>
+#include <iterator>
+#include <stdexcept>
 
 namespace chip8 {
 
 template <typename T, std::size_t sz> class Memory {
   public:
     Memory() = default;
+    Memory(T dv) {
+        auto it = begin(), stop = end();
+        for (; it != stop; ++it)
+            *it = dv;
+    }
+
     ~Memory() = default;
 
     T &operator[](std::size_t i) {
-        assert(i < sz);
+        if (i >= sz)
+            throw std::out_of_range(std::string("not in range: ", i));
         return m_buffer[i];
     }
 
     T operator[](std::size_t i) const {
-        assert(i < sz);
-        return m_buffer[i];
+        if (i >= sz)
+            throw std::out_of_range(std::string("not in range: ", i));
+        return this[i];
     }
 
+    T *operator&() { return &m_buffer[0]; }
+    const T *operator&() const { return &this; }
+
+    T *begin() { return std::begin(m_buffer); }
+    T *end() { return std::end(m_buffer); }
+
+    std::size_t size() const { return sz; }
+
   private:
-    std::array<T, sz> m_buffer;
+    T m_buffer[sz];
 };
 } // namespace chip8
 
