aca29c9c744c0c08c61b55effca565345b964c2e
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-08 18:56:24 -0500

    Wrap method defs in 'cpu' source file in the 'chip8' namespace

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,13 +2,13 @@
 
 #include <iostream>
 
-using namespace chip8;
-
-chip8::Cpu::opcode_t chip8::Cpu::fetch() {
+namespace chip8 {
+Cpu::opcode_t Cpu::fetch() {
     opcode_t op{};
 
     op |= m_ram[m_pc++] << 8;
     op |= m_ram[m_pc];
 
     return op;
+}
 }
