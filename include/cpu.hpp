2bf1aa5a5fcedf091b53bda3fd10a884a709841f
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-08 15:46:37 -0500

    Add 'Cpu' class definition in 'cpu' header file

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,4 +1,15 @@
 #ifndef CHIP8_CPU_HPP
 #define CHIP8_CPU_HPP
 
+#include <cstdint>
+#include "memory.hpp"
+
+namespace chip8 {
+class Cpu {
+  public:
+    Cpu() = default;
+    ~Cpu() = default;
+};
+} // namespace chip8
+
 #endif
