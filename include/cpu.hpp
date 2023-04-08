fa428aacf48bc962b7980745e19e8ed72c1fade6
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-08 18:38:55 -0500

    Format 'cpu' header file

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,16 +1,17 @@
 #ifndef CHIP8_CPU_HPP
 #define CHIP8_CPU_HPP
 
-#include <cstdint>
 #include "memory.hpp"
+#include <cstdint>
 
 namespace chip8 {
 class Cpu {
   public:
-    Cpu(Memory<std::uint8_t, 4096> &ram): m_ram{ram} {}
+    Cpu(Memory<std::uint8_t, 4096> &ram) : m_ram{ram} {}
     ~Cpu() = default;
 
     std::uint16_t fetch();
+
   private:
     Memory<std::uint8_t, 4096> &m_ram;
     std::uint16_t m_pc{0x200};
