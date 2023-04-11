2c876a713dd8b59acc7f323f5f97b1121041bc64
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-10 19:06:58 -0500

    Copy cpu-specific types to 'constants' header file

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,10 +1,22 @@
 #ifndef CHIP8_CONSTANTS_HPP
 #define CHIP8_CONSTANTS_HPP
 
+#include <cstdint>
+
 namespace chip8 {
+using byte_t = std::uint8_t;
+using opcode_t = std::uint16_t;
+using addr_t = std::uint16_t;
+using oper_t = int (*)(opcode_t);
+
+enum class cpu_status_t {
+    CPU_OK,
+    CPU_ERR,
+};
+
 inline constexpr int mem_sz{0x1000};
 inline constexpr int pc_init{0x200};
 inline constexpr int n_regs{0x10};
-}
+} // namespace chip8
 
 #endif
