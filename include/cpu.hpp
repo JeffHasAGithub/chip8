4243ecbf7e312c79abc7750c754458eed54e37a6
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-13 20:40:19 -0500

    Rename Cpu::m_ram member variable to Cpu::m_mem

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -19,13 +19,14 @@ class Cpu {
     cpu_status_t execute(oper_t);
 
   private:
-    Memory<byte_t, mem_sz> m_ram{};
+    Memory<byte_t, mem_sz> m_mem{};
 
     byte_t m_gp[n_regs]{}; // general purpose registers
     byte_t m_dt;           // delay timer
     byte_t m_st;           // sound timer
     addr_t m_pc{pc_init};  // program counter
     addr_t m_sp;           // stack pointer
+    addr_t m_i;            // index register
 };
 } // namespace chip8
 
