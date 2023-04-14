c2ed9d3e77fb59f851178917370378499cacf720
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-13 21:51:19 -0500

    Update cpu (#7)
    * Format 'Cpu' header file
    
    * Update Cpu::ram member variable to use byte_t as a template type parameter
    
    * Remove single parameter constructor for 'Cpu' class and add default constructor
    
    * Delete compiler generated copy constructor for 'Cpu' class
    
    * Delete compiler generated assignment operator for 'Cpu' class
    
    * Rename Cpu::m_ram member variable to Cpu::m_mem
    
    * Update Cpu::fetch definition to use Cpu::m_mem
    
    * Add single parameter constructor to 'Cpu' class that takes a rom filename
    
    * Replace Cpu::m_ram with Cpu::m_mem in Cpu::fetch member function
    
    * Rename 'pc_init' constexpr to 'prog_init'
    
    * Add 'prog_end' constexpr to 'common' header file
    
    * Change initliazer for Cpu::m_pc from 'pc_init' to 'prog_init'
    
    * Update 'Cpu' class single parameter constructor to take std::istream& instead of std::ifstream&
    
    * Update .gitignore

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -3,30 +3,37 @@
 
 #include "common.hpp"
 #include "memory.hpp"
-#include <cstdint>
+#include <iostream>
 
 namespace chip8 {
-
 class Cpu {
   public:
-    Cpu(Memory<std::uint8_t, mem_sz> &ram) : m_ram{ram} {}
+    Cpu(std::istream &rom) {
+        while (rom && m_pc < prog_end)
+            rom >> m_mem[m_pc++];
+
+        if (!rom.eof())
+            throw std::runtime_error("failure reading rom");
+    }
+    Cpu(const Cpu &cpu) = delete;
+
     ~Cpu() = default;
+
+    Cpu &operator=(const Cpu &cpu) = delete;
 
     opcode_t fetch();
     oper_t decode(opcode_t);
     cpu_status_t execute(oper_t);
 
   private:
-    Memory<std::uint8_t, mem_sz> &m_ram;
+    Memory<byte_t, mem_sz> m_mem{};
 
-    // general purpose registers
-    byte_t m_gp[n_regs]{};
-
-    byte_t m_dt; // delay timer
-    byte_t m_st; // sound timer
-
-    addr_t m_pc{pc_init}; // program counter
-    addr_t m_sp;          // stack pointer
+    byte_t m_gp[n_regs]{};  // general purpose registers
+    byte_t m_dt;            // delay timer
+    byte_t m_st;            // sound timer
+    addr_t m_pc{prog_init}; // program counter
+    addr_t m_sp;            // stack pointer
+    addr_t m_i;             // index register
 };
 } // namespace chip8
 
--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,7 +1,12 @@
 #include "chip8.hpp"
 
+#include <iostream>
+#include <new>
+
 namespace chip8 {
 chip8_status_t run() {
+    Cpu cpu;
+
     return chip8_status_t::CHIP8_OK;
 }
 } // namespace chip8
