f64fe79f5ca85d02e162c661f40dea10bb8346e5
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-15 17:24:09 -0500

    Add 'Stack' class template definition

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,4 +1,14 @@
 #ifndef CHIP8_STACK_HPP
 #define CHIP8_STACK_HPP
 
+#include "memory.hpp"
+
+namespace chip8 {
+template <typename T, std::size_t sz>
+class Stack: public Memory<T, sz> {
+    Stack() = default; 
+    ~Stack() = default;
+};
+}
+
 #endif
