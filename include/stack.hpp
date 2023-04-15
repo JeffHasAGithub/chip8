f805a02fe095ddabf6308ca5c74ad435e11caa1d
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-15 17:38:14 -0500

    Add Stack<T, std::size_t>::push(T) member function

--- include/stack.hpp
+++ include/stack.hpp
@@ -3,6 +3,7 @@
 
 #include "common.hpp"
 #include "memory.hpp"
+#include <stdexcept>
 
 namespace chip8 {
 template <typename T, std::size_t sz>
@@ -10,6 +11,12 @@ class Stack: public Memory<T, sz> {
 public:
     Stack() = default; 
     ~Stack() = default;
+
+    void push(T val) {
+        if (m_sp == 0)
+            throw std::out_of_range("bottom of the stack");
+        this[m_sp--] = val;
+    }
 private:
     std::size_t m_sp{sz - 1};
 };
