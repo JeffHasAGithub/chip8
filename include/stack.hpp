ef9b3334f4d3cfa97253926333625e0d604540fe
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-15 17:28:47 -0500

    Add Stack<T, std::size_t>::m_sp member variable

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,13 +1,17 @@
 #ifndef CHIP8_STACK_HPP
 #define CHIP8_STACK_HPP
 
+#include "common.hpp"
 #include "memory.hpp"
 
 namespace chip8 {
 template <typename T, std::size_t sz>
 class Stack: public Memory<T, sz> {
+public:
     Stack() = default; 
     ~Stack() = default;
+private:
+    std::size_t m_sp{sz - 1};
 };
 }
 
