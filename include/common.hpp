262bb6a7c885f4dadf084f38d9e99e8986ff5cd9
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-16 18:19:25 -0500

    Add stack to cpu (#11)
    * Rename Cpu::m_mem memvar to Cpu::m_ram
    
    * Add 'Cpu::m_stk' memvar for holding addresses pushed onto the stack
    
    * Add 'stk_sz' inline constexpr to 'common' header file

--- include/common.hpp
+++ include/common.hpp
@@ -20,6 +20,7 @@ enum class cpu_status_t {
 };
 
 inline constexpr int mem_sz{0x1000};
+inline constexpr int stk_sz{0x10};
 inline constexpr int prog_init{0x200};
 inline constexpr int prog_end{0x600};
 inline constexpr int n_regs{0x10};
