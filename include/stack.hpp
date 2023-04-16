91911994c1217bf43b7dd566c8e78bc8e45d2355
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-15 19:56:54 -0500

    Add stack (#9)
    * Add 'stack' header file
    
    * Add 'Stack' class template definition
    
    * Add Stack<T, std::size_t>::m_sp member variable
    
    * Add Stack<T, std::size_t>::push(T) member function
    
    * Update Stack<T, std::size_t>::push(T) member function to decrement stack pointer before push
    
    * Add Stack<T, std::size_t>::pop() member function
    
    * Update error message in Stack<T, std::size_t>::push() member function
    
    * Add Stack<T, std::size_t>::Stack(T) ctor that calls base ctor
    
    * Add Stack<T, std::size_t>::sp() member function
    
    * Add tests for Stack<T, std::size_t> class template
    
    * Update .clang-format

---
+++ include/stack.hpp
@@ -0,0 +1,35 @@
+#ifndef CHIP8_STACK_HPP
+#define CHIP8_STACK_HPP
+
+#include "memory.hpp"
+#include <stdexcept>
+
+namespace chip8 {
+template <typename T, std::size_t sz>
+class Stack : public Memory<T, sz> {
+  public:
+    Stack() = default;
+    Stack(T dv) : Memory<T, sz>(dv){};
+
+    ~Stack() = default;
+
+    void push(T val) {
+        if (m_sp < 0)
+            throw std::out_of_range("beyond stack range");
+        (*this)[--m_sp] = val;
+    }
+
+    T pop() {
+        if (m_sp >= sz)
+            throw std::out_of_range("beyond stack range");
+        return (*this)[m_sp++];
+    }
+
+    std::size_t sp() const { return m_sp; }
+
+  private:
+    std::size_t m_sp{sz};
+};
+} // namespace chip8
+
+#endif
