c651de3fda86297ab390f6c6e6957638ad13e6cb
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-21 21:42:18 -0500

    Define Args::add(const vector<string>&, const Value&, const string&) member function

--- src/args.cpp
+++ src/args.cpp
@@ -3,6 +3,10 @@
 Args *Args::s_instance{nullptr};
 std::mutex Args::s_mutex{};
 
+void Args::add(const std::vector<std::string> &flags, const Value &val, const std::string &help) {
+    m_args.push_back(Arg{flags, val, help});
+}
+
 Args *Args::instance() {
     std::lock_guard<std::mutex> lock{s_mutex};
     if (s_instance == nullptr)
