221048c00d8eef2018df113c18803d5f820bf232
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-21 19:26:35 -0500

    Add 'Args' class definition to 'args' header file

--- include/args.hpp
+++ include/args.hpp
@@ -1,4 +1,20 @@
 #ifndef ARGS_HPP
 #define ARGS_HPP
 
+#include <mutex>
+
+class Args {
+  public:
+    Args(const Args &) = delete;
+    Args &operator=(const Args &) = delete;
+
+    static Args &instance(int argc, char *argv[]);
+
+  private:
+    Args(int argc, char *argv[]);
+
+    static Args *s_instance;
+    static std::mutex s_mutex;
+};
+
 #endif
