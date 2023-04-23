fccdcce119746873dd547e45d313296adcbaac79
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-22 21:11:51 -0500

    Fixed bug where Args::parse(int, char**) when first flag checked was invalid

--- src/args.cpp
+++ src/args.cpp
@@ -20,28 +20,29 @@ void Args::parse(int argc, char *argv[]) {
             flag = flag.substr(0, sep_pos);
         }
 
+        bool found{false};
         for (auto const &arg : m_args) {
             if (std::find(arg.m_flags.begin(), arg.m_flags.end(), flag) !=
                 arg.m_flags.end()) {
+                found = true;
 
-                if (std::holds_alternative<bool*>(arg.m_value)) {
+                if (std::holds_alternative<bool *>(arg.m_value)) {
                     if (val.empty() || val == "true" || val == "false")
-                        *std::get<bool*>(arg.m_value) = (val != "false");
-                } else if (std::holds_alternative<std::string*>(arg.m_value)) {
-                    *std::get<std::string*>(arg.m_value) = val;
+                        *std::get<bool *>(arg.m_value) = (val != "false");
+                } else if (std::holds_alternative<std::string *>(arg.m_value)) {
+                    *std::get<std::string *>(arg.m_value) = val;
                 } else {
                     std::visit(
-                        [&val](auto&& arg) {
+                        [&val](auto &&arg) {
                             std::stringstream ss{val};
                             ss >> *arg;
                         },
-                        arg.m_value
-                    );
+                        arg.m_value);
                 }
-            } else {
-                throw ArgsException("invalid flag - " + flag);
             }
         }
+        if (!found)
+            throw ArgsException("invalid flag - " + flag);
     }
 }
 
