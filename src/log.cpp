97e7289dd0c616e0c3786039b9bedd208be0bfe6
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-23 16:50:07 -0500

    Declare/Define Log::info(const string&) member function

--- src/log.cpp
+++ src/log.cpp
@@ -7,6 +7,10 @@ void Log::write(const std::string &msg) {
     m_ostream << msg;
 }
 
+void Log::info(const std::string &msg) {
+    write("[INFO] " + msg); 
+}
+
 Log *Log::instance(std::ostream &out) {
     if (s_instance == nullptr)
         s_instance = new Log(out);
