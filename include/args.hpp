b3fd5478f28bbb6e6021cc5db3efbf6ecb6d02a4
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-22 21:23:43 -0500

    Add args parser (#14)
    * Add 'args' source/header files
    
    * Add 'Args' class definition to 'args' header file
    
    * Define Args::s_instance and Args::s_mutex static member variables
    
    * Declare Args::Args() default constructor
    
    * Change return value of Args::instance(int, char**) static member function to Args*
    
    * Remove declaration of Args::Args(int, char**) and update params of Args::instance static member function
    
    * Use compiler generated Args::Args() constructor
    
    * Define Args::instance static member function
    
    * Add tests for 'Args' class
    
    * Update makefile
    
    * Define Args::Value type alias
    
    * Define Args::Arg struct
    
    * Declare Args::add(const vector<string>&, const Value&, const std::string&) member function
    
    * Define Args::m_args member variable
    
    * Define Args::add(const vector<string>&, const Value&, const string&) member function
    
    * Add 'chip8' namespace to 'args' header/source files
    
    * Declare Args::parse(int, char**) member function
    
    * Args::parse(int, char**) declaration now returns void
    
    * Define Args::parse(int, char**) member function
    
    * index var of top most loop in Args::parse(int, char**) starts at 1
    
    * Remove unused #include statements
    
    * Define ArgsException class that inherits from std::runtime_error
    
    * Throw ArgsException in Args::parse(int, char**) if bad flag is passed to member function
    
    * Fixed bug where Args::parse(int, char**) when first flag checked was invalid
    
    * Declare Args::clear() member function
    
    * Define Args::clear() member function
    
    * Update tests

---
+++ include/args.hpp
@@ -0,0 +1,47 @@
+#ifndef ARGS_HPP
+#define ARGS_HPP
+
+#include <algorithm>
+#include <mutex>
+#include <sstream>
+#include <stdexcept>
+#include <variant>
+#include <vector>
+
+namespace chip8 {
+class ArgsException : public std::runtime_error {
+  public:
+    ArgsException(const std::string &msg) : std::runtime_error{msg} {};
+};
+
+class Args {
+  public:
+    using Value = std::variant<int *, double *, bool *, std::string *>;
+
+    Args(const Args &) = delete;
+    Args &operator=(const Args &) = delete;
+
+    void add(const std::vector<std::string> &flags, const Value &val,
+             const std::string &help);
+    void clear();
+    void parse(int argc, char *argv[]);
+
+    static Args *instance();
+
+  private:
+    struct Arg {
+        std::vector<std::string> m_flags;
+        Value m_value;
+        std::string m_help;
+    };
+
+    Args() = default;
+
+    std::vector<Arg> m_args{};
+
+    static Args *s_instance;
+    static std::mutex s_mutex;
+};
+} // namespace chip8
+
+#endif
---
+++ src/args.cpp
@@ -0,0 +1,57 @@
+#include "args.hpp"
+
+namespace chip8 {
+Args *Args::s_instance{nullptr};
+std::mutex Args::s_mutex{};
+
+void Args::add(const std::vector<std::string> &flags, const Value &val,
+               const std::string &help) {
+    m_args.push_back(Arg{flags, val, help});
+}
+
+void Args::clear() { m_args.clear(); }
+
+void Args::parse(int argc, char *argv[]) {
+    for (int i{1}; i < argc; ++i) {
+        std::string flag{argv[i]};
+        std::string val{};
+
+        std::size_t sep_pos = flag.find('=');
+        if (sep_pos != std::string::npos) {
+            val = flag.substr(sep_pos + 1);
+            flag = flag.substr(0, sep_pos);
+        }
+
+        bool found{false};
+        for (auto const &arg : m_args) {
+            if (std::find(arg.m_flags.begin(), arg.m_flags.end(), flag) !=
+                arg.m_flags.end()) {
+                found = true;
+
+                if (std::holds_alternative<bool *>(arg.m_value)) {
+                    if (val.empty() || val == "true" || val == "false")
+                        *std::get<bool *>(arg.m_value) = (val != "false");
+                } else if (std::holds_alternative<std::string *>(arg.m_value)) {
+                    *std::get<std::string *>(arg.m_value) = val;
+                } else {
+                    std::visit(
+                        [&val](auto &&arg) {
+                            std::stringstream ss{val};
+                            ss >> *arg;
+                        },
+                        arg.m_value);
+                }
+            }
+        }
+        if (!found)
+            throw ArgsException("invalid flag - " + flag);
+    }
+}
+
+Args *Args::instance() {
+    std::lock_guard<std::mutex> lock{s_mutex};
+    if (s_instance == nullptr)
+        s_instance = new Args();
+    return s_instance;
+}
+} // namespace chip8
