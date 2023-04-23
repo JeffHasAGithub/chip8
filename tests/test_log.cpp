85b7294b470b29209f5dd4306dd35132b747ba6e
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-23 17:34:28 -0500

    Update tests

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -1,16 +1,17 @@
 #include <catch2/catch.hpp>
 #include <stdexcept>
+#include <sstream>
 
 #include "log.hpp"
 using namespace chip8;
 
 TEST_CASE("Log: adheres to singleton pattern", "[log]") {
-    SECTION("Log::instance() creates singleton on first invocation") {
+    SECTION("creates singleton on first invocation") {
         Log *log = Log::instance();
         REQUIRE(log != nullptr);
     }
 
-    SECTION("Log: multiple invocations for Log::instance() returns same pointer") {
+    SECTION("multiple invocations for Log::instance() returns same pointer") {
         Log *args = Log::instance();
         REQUIRE(args == Log::instance());
     }
