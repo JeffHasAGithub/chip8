a6e3835dd28151a0e8fc750b730d9eff3b7f5dc7
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-23 15:54:04 -0500

    Add 'Log' class definition

--- include/log.hpp
+++ include/log.hpp
@@ -1,6 +1,21 @@
 #ifndef CHIP8_LOG_HPP
 #define CHIP8_LOG_HPP
 
-namespace chip8 {}
+#include <iostream>
+
+namespace chip8 {
+class Log {
+  public:
+    Log(const Log &log) = delete;
+    Log &operator=(const Log &log) = delete;
+
+    static Log *instance(std::ostream &out = std::cout);
+
+  private:
+    Log(std::ostream &out);
+
+    static Log *s_instance;
+};
+} // namespace chip8
 
 #endif
