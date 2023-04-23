e7f1eaa60f9e076a49e497ec561b9b10314e48d8
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-23 16:07:26 -0500

    Define 'Log::instance(std::ostream&) member function

--- src/log.cpp
+++ src/log.cpp
@@ -1,3 +1,11 @@
 #include "log.hpp"
 
-namespace chip8 {}
+namespace chip8 {
+Log *s_instance{nullptr};
+
+Log *Log::instance(std::ostream &out) {
+    if (s_instance == nullptr)
+        s_instance = new Log(out);
+    return s_instance;
+}
+} // namespace chip8
