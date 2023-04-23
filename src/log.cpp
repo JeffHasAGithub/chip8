60eaaa616940b9365ad7f71526e3870589a33a01
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-23 16:34:32 -0500

    Define Log::write(string_view) member function

--- src/log.cpp
+++ src/log.cpp
@@ -3,6 +3,10 @@
 namespace chip8 {
 Log *Log::s_instance{nullptr};
 
+void Log::write(std::string_view sv) {
+    m_ostream << sv;
+}
+
 Log *Log::instance(std::ostream &out) {
     if (s_instance == nullptr)
         s_instance = new Log(out);
