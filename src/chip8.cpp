f88821161f7af0896901b86a863de3331ab05bd1
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-24 21:35:33 -0500

    Update chip8::run(int, char**) definition

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,11 +1,11 @@
 #include "chip8.hpp"
 
 #include <iostream>
-#include <new>
 
 namespace chip8 {
-chip8_status_t run() {
-    Cpu cpu;
+chip8_status_t run(int argc, char *argv[]) {
+    Args *args{Args::instance()};
+    Log *log{Log::instance()};
 
     return chip8_status_t::CHIP8_OK;
 }
