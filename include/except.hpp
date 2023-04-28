22856e624ac8dd5437d73949aeca82900433e31a
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-27 19:56:09 -0500

    Add docs to 'except' header file

--- include/except.hpp
+++ include/except.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file except.hpp
+ *
+ * The Chip8Exception class provides a common
+ * interface for all exceptions thrown from
+ * the various emulator subsystems. It is a
+ * child of the std::runtime_error class.
+ */
+
 #ifndef CHIP8_EXCEPT_HPP
 #define CHIP8_EXCEPT_HPP
 
