07b4fd2d031be6ef2658cba4e02b47f921d8c0ff
Jeff Moore <jeffmooreweb@gmail.com>
2023-04-27 20:14:06 -0500

    Add Exception class to 'except' header file

--- include/except.hpp
+++ include/except.hpp
@@ -10,4 +10,15 @@
 #ifndef CHIP8_EXCEPT_HPP
 #define CHIP8_EXCEPT_HPP
 
+#include <stdexcept>
+
+namespace chip8 {
+class Exception : public std::runtime_error {
+  public:
+    Exception(const std::string &msg, int err_c = 0)
+        : std::runtime_error{msg}, err_c{err_c} {};
+    int err_c;
+};
+} // namespace chip8
+
 #endif
