8fd9207794b46b55fca5f62f0444fe5c8f06e3de
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-02 18:55:56 -0500

    Add bits::extract(std::size_t, std::size_t) function decl/def

--- include/bits.hpp
+++ include/bits.hpp
@@ -1,6 +1,12 @@
 #ifndef CHIP8_BITS_HPP
 #define CHIP8_BITS_HPP
 
-namespace chip8 {}
+#include <cstddef>
+
+namespace chip8 {
+namespace bits {
+std::size_t extract(std::size_t val, std::size_t mask);
+}
+} // namespace chip8
 
 #endif
--- src/bits.cpp
+++ src/bits.cpp
@@ -1,1 +1,9 @@
 #include "bits.hpp"
+
+namespace chip8 {
+namespace bits {
+std::size_t extract(std::size_t val, std::size_t mask) {
+    return val & mask;
+}
+} // namespace bits
+} // namespace chip8
