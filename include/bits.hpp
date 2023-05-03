587f3abe6621a17cc2fb42c652fbc827566438e4
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-02 19:23:08 -0500

    Add bits::flip(std::size_t, std::size_t) function decl/def

--- include/bits.hpp
+++ include/bits.hpp
@@ -8,7 +8,8 @@ namespace bits {
 std::size_t extract(std::size_t val, std::size_t mask);
 std::size_t on(std::size_t val, std::size_t mask);
 std::size_t off(std::size_t val, std::size_t mask);
-}
+std::size_t flip(std::size_t val, std::size_t mask);
+} // namespace bits
 } // namespace chip8
 
 #endif
--- src/bits.cpp
+++ src/bits.cpp
@@ -13,5 +13,9 @@ std::size_t on(std::size_t val, std::size_t mask) {
 std::size_t off(std::size_t val, std::size_t mask) {
     return val & ~mask;
 }
+
+std::size_t flip(std::size_t val, std::size_t mask) {
+    return val ^ mask;
+}
 } // namespace bits
 } // namespace chip8
