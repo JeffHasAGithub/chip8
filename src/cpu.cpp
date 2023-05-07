5b4f916194e063a619744cb4eccc9d76b7a2d09d
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-06 19:36:23 -0500

    Add defs for chip8 cpu instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,6 +1,30 @@
 #include "cpu.hpp"
 
 namespace chip8 {
+Cpu::CpuStatus op_0nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_1nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_2nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_3nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_4nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_5nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_6nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_7nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_8nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_9nnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Annn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Bnnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Cnnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Dnnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Ennn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Fnnn(Cpu&) { return Cpu::CpuStatus::CPU_OK; }
+
+static std::array<Cpu::Oper, 0x10> opers{
+    op_0nnn, op_1nnn, op_2nnn, op_3nnn, op_4nnn,
+    op_5nnn, op_6nnn, op_7nnn, op_8nnn, op_9nnn,
+    op_Annn, op_Bnnn, op_Cnnn, op_Dnnn, op_Ennn,
+    op_Fnnn
+};
+
 Cpu::Cpu(std::istream &rom) {
     int status = init_ram(rom);
 
