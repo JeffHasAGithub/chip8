c68701aafaff58f8587449ca96f9b4914bfdf616
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-07 17:29:45 -0500

    Define 'extract_x' static func in 'opers' source

--- src/opers.cpp
+++ src/opers.cpp
@@ -10,6 +10,10 @@ static std::size_t extract_mask(Addr addr, Addr mask) {
     return Bits{addr}.extract(mask);
 }
 
+static std::size_t extract_x(Addr addr) {
+    return extract_mask(addr, x_mask) >> 8;
+}
+
 Cpu::CpuStatus op_0nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
 
 // Jump to location 'nnn'
