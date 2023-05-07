7748825ead332dbc41ba699bed8c96ce1bc9e46b
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-06 19:45:24 -0500

    Add defs for cpu instructions in 'opers' source

--- src/opers.cpp
+++ src/opers.cpp
@@ -1,3 +1,20 @@
 #include "opers.hpp"
 
-namespace chip8 {}
+namespace chip8 {
+Cpu::CpuStatus op_0nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_1nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_2nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_3nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_4nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_5nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_6nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_7nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_8nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_9nnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Annn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Bnnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Cnnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Dnnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Ennn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+Cpu::CpuStatus op_Fnnn(Cpu &) { return Cpu::CpuStatus::CPU_OK; }
+}
