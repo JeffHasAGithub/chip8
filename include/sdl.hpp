7fdff6bd742368a37cdfafd69da13fa7cd326505
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-09 21:15:37 -0500

    Define SdlException class

--- include/sdl.hpp
+++ include/sdl.hpp
@@ -10,13 +10,20 @@
 #ifndef CHIP8_SDL_HPP
 #define CHIP8_SDL_HPP
 
+#include "except.hpp"
+
 namespace chip8 {
+class SdlException : public Exception {
+  public:
+    SdlException(const std::string &msg, int err_c) : Exception{msg, err_c} {};
+};
+
 class Sdl {
   public:
     Sdl(const Sdl &sdl) = delete;
     Sdl &operator=(const Sdl &sdl) = delete;
 
-    static Sdl *instance();
+    static Sdl *instance(int width, int height);
 
   private:
     Sdl(int width, int height) : m_width{width}, m_height{height} {};
