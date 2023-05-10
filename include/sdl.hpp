765bca2f738f03e40de942b413d4d17c62682d5e
Jeff Moore <jeffmooreweb@gmail.com>
2023-05-09 21:02:31 -0500

    Define Sdl class

--- include/sdl.hpp
+++ include/sdl.hpp
@@ -1,6 +1,21 @@
 #ifndef CHIP8_SDL_HPP
 #define CHIP8_SDL_HPP
 
-namespace chip8 {}
+namespace chip8 {
+class Sdl {
+  public:
+    Sdl(const Sdl &sdl) = delete;
+    Sdl &operator=(const Sdl &sdl) = delete;
+
+    static Sdl *instance();
+
+  private:
+    Sdl(int width, int height) : m_width{width}, m_height{height} {};
+
+    int m_width;
+    int m_height;
+    static Sdl *s_instance;
+};
+} // namespace chip8
 
 #endif
--- src/sdl.cpp
+++ src/sdl.cpp
@@ -1,3 +1,11 @@
 #include "sdl.hpp"
 
-namespace chip8 {}
+namespace chip8 {
+Sdl *Sdl::s_instance{nullptr};
+
+Sdl *Sdl::instance(int w_width, int w_height) {
+    if (s_instance == nullptr)
+        s_instance = new Sdl(w_width, w_height);
+    return s_instance;
+}
+} // namespace chip8
